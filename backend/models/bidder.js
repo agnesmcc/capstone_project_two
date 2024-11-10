4259d6c1a09e17225ab7e556b49cd0adb8c7bbd2
Agnes <agnes.mccall5@gmail.com>
2024-11-09 19:58:50 -0500

    add bidder model

---
+++ backend/models/bidder.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const db = require("../db");
+const { BadRequestError, NotFoundError } = require("../expressError");
+
+class Bidder {
+    static async getBidsByListingId(listing_id) {
+        const result = await db.query(
+            `SELECT *
+             FROM bidders
+             WHERE listing_id = $1`,
+            [listing_id]
+        );
+        return result.rows;
+    }
+
+    static async getBidsByBidder(username) {
+        const result = await db.query(
+            `SELECT *
+             FROM bidders
+             WHERE bidder = $1`,
+            [username]
+        );
+        return result.rows;
+    }
+
+    static async addBid(bidder, listing_id, bid) {
+        const result = await db.query(
+            `INSERT INTO bidders (bidder, listing_id, bid) VALUES ($1, $2, $3) 
+            RETURNING bidder, listing_id, bid`,
+            [bidder, listing_id, bid]
+        );
+        return result.rows[0];
+    }
+
+    static async removeBid(bidder, listing_id) {
+        const result = await db.query(
+            `DELETE FROM bidders WHERE bidder = $1 AND listing_id = $2 RETURNING bidder, listing_id`,
+            [bidder, listing_id]
+        );
+        return result.rows[0];
+    }
+}
+
+module.exports = Bidder;
---
+++ backend/models/bidder.test.js
@@ -0,0 +1,73 @@
+"use strict";
+
+const db = require("../db.js");
+const { BadRequestError, NotFoundError } = require("../expressError.js");
+const Listing = require("./listing.js");
+const Category = require("./category.js");
+const User = require("./user.js");
+const Bidder = require("./bidder.js");
+
+const testListing = {
+    created_by: "testUser",
+    title: "My Test Listing",
+    description: "a nice couch",
+    image: "couch.jpg",
+    starting_bid: "500.00",
+    category: "furniture",
+    end_datetime: new Date('2022-01-01T05:00:00.000Z')
+}
+
+beforeEach(async () => {
+    await db.query("DELETE FROM listings");
+    await db.query("DELETE FROM categories");
+    await db.query("DELETE FROM users");
+    await Category.addCategory("furniture");
+    await User.register({
+        username: "testUser",
+        firstName: "Test",
+        lastName: "User",
+        password: "testUser",
+        email: "a@b.com"
+    })
+});
+
+afterAll(async () => {
+    await db.end();
+});
+
+describe('Bidder', () => {
+    test("getBidsByListingId", async () => {
+        const listing = await Listing.addListing(testListing);
+        const result = await Bidder.getBidsByListingId(listing.id);
+        expect(result).toEqual([]);
+    });
+
+    test("getBidsByBidder", async () => {
+        const listing = await Listing.addListing(testListing);
+        await Bidder.addBid("testUser", listing.id, "100.00");
+        const result = await Bidder.getBidsByBidder("testUser");
+        expect(result).toMatchObject([{"bidder": "testUser", "listing_id": listing.id, "bid": "100.00"}]);
+    });
+
+    test("addBidder fails with no bid", async () => {    
+        const listing = await Listing.addListing(testListing);
+        expect(async () => {
+            await Bidder.addBid("testUser", listing.id, null)
+        }).rejects.toThrow();
+    });
+
+    test("addBidder", async () => {
+        const listing = await Listing.addListing(testListing);
+        const result = await Bidder.addBid("testUser", listing.id, "100.00");
+        expect(result).toEqual({"bidder": "testUser", "listing_id": listing.id, "bid": "100.00"});
+    })
+
+    test("removeBidder", async () => {
+        const listing = await Listing.addListing(testListing);
+        await Bidder.addBid("testUser", listing.id, "100.00");
+        const result = await Bidder.removeBid("testUser", listing.id);
+        expect(result).toEqual({"bidder": "testUser", "listing_id": listing.id});
+        const allBidders = await Bidder.getBidsByListingId(listing.id);
+        expect(allBidders).toEqual([]);
+    }) 
+});
