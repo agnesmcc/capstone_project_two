0b41ac78d391de028f76ca6073149e2edbc2e3ff
Agnes <agnes.mccall5@gmail.com>
2024-12-22 21:07:02 -0500

    add CORS config

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,11 @@ const authRoutes = require("./routes/auth");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(morgan("tiny"));
 
